b03b0a8541990aecf937abf53e9e56a6defd1212
Chris <86952358+pickiee@users.noreply.github.com>
2021-09-15 13:46:22 +0100

    uploads

---
+++ src/templates/_trademark.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @name:       Samp Front
+ * @version:    0.5.1
+ * @author:     EOussama (eoussama.github.io)
+ * @license     MIT
+ * @source:     github.com/EOussama/samp-front
+ * 
+ * This is the trademark footer that's appened to the bottom of
+ * each page to link to the github repo of Samp Front.
+ */
+?>
+
+<div class="ui basic segment trademark">
+    <small>
+        <a href="https://github.com/EOussama/samp-front">
+            Samp Front
+        </a>
+        by
+        <a href="https://github.com/EOussama">
+            EOussama
+        </a>
+    </small>
+</div>
